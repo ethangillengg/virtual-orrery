e420489feee15e1c1910a9facdafcaaa052943b3
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-28 17:31:25 -0700

    feat: depth testing + functional requirement 1

--- assets/shaders/starter.frag.glsl
+++ assets/shaders/starter.frag.glsl
@@ -26,14 +26,18 @@ pc;
 layout(binding = 0) uniform sampler2D textures[MAX_TEXTURES];
 
 // Material properties
-vec3 bg_color = vec3(0.00, 0.00, 0.05);
-void drawSphere(vec3 centerPos, int texIndex) {
+vec3 bg_color = vec3(0.00, 0.00, 0.5);
+// For depth testing
+float largestT = 99999999;
+
+void drawSphere(vec3 centerPos, float radius, int texIndex) {
   // intersect against sphere of radius 1 centered at the origin
   vec3 dir = normalize(d);
 
-  float prod = 2.0 * dot(centerPos, dir);
-  float normp = length(centerPos);
-  float discriminant = prod * prod - 4.0 * (-1.0 + normp * normp);
+  float prod = dot(2.0 * dir, (p - centerPos));
+  float normp = length(p - centerPos);
+  float c = (normp * normp) - (radius * radius);
+  float discriminant = (prod * prod) - (4.0 * c);
   if (discriminant >= 0.0) {
     // determine intersection point
     float t1 = 0.5 * (-prod - sqrt(discriminant));
@@ -49,8 +53,16 @@ void drawSphere(vec3 centerPos, int texIndex) {
     }
     if (tmax > 0.0) {
       t = (tmin > 0) ? tmin : tmax;
-      vec3 ipoint = centerPos + t * (dir);
-      vec3 normal = normalize(ipoint);
+
+      // Don't draw if the fragment is behind something else
+      if (t > largestT) {
+        return;
+      } else {
+        largestT = t;
+      }
+
+      vec3 ipoint = (p + (t * dir));
+      vec3 normal = normalize(ipoint - centerPos);
 
       // determine texture coordinates in spherical coordinates
 
@@ -68,6 +80,7 @@ void drawSphere(vec3 centerPos, int texIndex) {
       // normalize coordinates for texture sampling.
       // Top-left of texture is (0,0) in Vulkan, so we can stick to spherical
       // coordinates
+      // color = vec4(vec3(closestZ), 1.0);
       color = texture(textures[int(mod(texIndex, MAX_TEXTURES))],
                       vec2(1.0 + 0.5 * theta / PI, phi / PI));
     }
@@ -77,7 +90,10 @@ void drawSphere(vec3 centerPos, int texIndex) {
 void main() {
 
   color = vec4(bg_color, 1.0);
-  // test
-  drawSphere(p - 1.5, TEXTURE_EARTH);
-  drawSphere(p, TEXTURE_SUN);
+
+  // So that it does not get cut off
+  drawSphere(vec3(0), largestT * 0.99, TEXTURE_STARS);
+  drawSphere(vec3(0), 0.5, TEXTURE_SUN);
+  drawSphere(vec3(0.0, 0.0, 0.9), 0.2, TEXTURE_EARTH);
+  drawSphere(vec3(0.0, 0.0, 1.3), 0.1, TEXTURE_MOON);
 }
