3e6a8203f9326ebcac6281dc4d95ba3fc013ed7f
Ethan Gill <ethan.gill@ucalgary.ca>
2023-11-29 09:30:55 -0700

    refactor: celestial struct + orbit functions

--- assets/shaders/starter.frag.glsl
+++ assets/shaders/starter.frag.glsl
@@ -4,12 +4,7 @@
 // save some space in the push constants by hard-coding this
 #define MAX_TEXTURES 4
 
-#define TEXTURE_STARS 0
-#define TEXTURE_SUN 1
-#define TEXTURE_EARTH 2
-#define TEXTURE_MOON 3
-
-#define TEXTURE_STARS 0
+#define TEXTURE_SKYBOX 0
 #define TEXTURE_SUN 1
 #define TEXTURE_EARTH 2
 #define TEXTURE_MOON 3
@@ -19,18 +14,8 @@
 
 #define ORBITAL_PERIOD_MOON 27.0
 #define ORBITAL_PERIOD_EARTH (12.0 * ORBITAL_PERIOD_MOON)
-
-// #define MODEL_RADIUS_SCALE 99999
-// #define MODEL_DISTANCE_SCALE 999999
-// volumetric mean radii in Km as specified in:
 // (https://nssdc.gsfc.nasa.gov/planetary/factsheet/moonfact.html)
 // (https://nssdc.gsfc.nasa.gov/planetary/factsheet/sunfact.html)
-// #define RADIUS_SUN 695700.0 / 10 / MODEL_RADIUS_SCALE
-// #define RADIUS_EARTH 6378.1 / MODEL_RADIUS_SCALE
-// #define RADIUS_MOON 1738.1 / MODEL_RADIUS_SCALE
-//
-// #define DISTANCE_EARTH 14960000 / MODEL_DISTANCE_SCALE
-// #define DISTANCE_MOON (378000 / MODEL_DISTANCE_SCALE) + DISTANCE_EARTH
 
 layout(location = 0) out vec4 color;
 
@@ -48,18 +33,75 @@ pc;
 
 layout(binding = 0) uniform sampler2D textures[MAX_TEXTURES];
 
-// Material properties
-vec3 bg_color = vec3(0.00, 0.00, 0.5);
-// For depth testing
-float largestT = 999999999;
+const vec3 bg_color = vec3(0.00, 0.00, 0.5);
+float largestT = 999999999; // For depth testing
 
-void drawSphere(vec3 centerPos, float radius, int texIndex) {
+struct Celestial {
+  vec3 pos;
+  float radius;
+  int texIndex;
+  float axialPeriod; // 0 for no axial rotation
+};
+
+void drawCelestial(Celestial celestial);
+vec3 orbitAbout(Celestial orbited, float orbitalPeriod, float orbitalRadius);
+
+void main() {
+  color = vec4(bg_color, 1.0);
+
+  Celestial skybox = {
+      vec3(0),         //
+      largestT * 0.99, // So that it does not get cut off
+      TEXTURE_SKYBOX,  //
+      0.0              // no axial rotation
+  };
+  drawCelestial(skybox);
+
+  Celestial sun = {
+      vec3(0),              //
+      0.3,                  //
+      TEXTURE_SUN,          //
+      AXIAL_PERIOD_SUN_MOON //
+
+  };
+  drawCelestial(sun);
+
+  Celestial earth = {
+      orbitAbout(sun, ORBITAL_PERIOD_EARTH, 1.5), // orbit of Earth -> Sun
+      0.2,                                        //
+      TEXTURE_EARTH,                              //
+      AXIAL_PERIOD_EARTH                          //
+  };
+  drawCelestial(earth);
+
+  Celestial moon = {
+      orbitAbout(earth, ORBITAL_PERIOD_MOON,
+                 0.5),       // orbit of Moon -> Earth
+      earth.radius * 0.2727, // roughly
+      TEXTURE_MOON,          //
+      AXIAL_PERIOD_SUN_MOON  //
+  };
+  drawCelestial(moon);
+}
+
+vec3 orbitAbout(Celestial orbited, float orbitalPeriod, float orbitalRadius) {
+
+  // accounts for current orbital position based on time
+  vec3 orbitingCenter = orbitalRadius * vec3(cos(pc.time / orbitalPeriod), 0.0,
+                                             sin(pc.time / orbitalPeriod));
+  // since we are orbiting about the orbited body,
+  // we need to account for its position
+  orbitingCenter += orbited.pos;
+  return orbitingCenter;
+}
+
+void drawCelestial(Celestial celestial) {
   // intersect against sphere of radius 1 centered at the origin
   vec3 dir = normalize(d);
 
-  float prod = dot(2.0 * dir, (p - centerPos));
-  float normp = length(p - centerPos);
-  float c = (normp * normp) - (radius * radius);
+  float prod = dot(2.0 * dir, (p - celestial.pos));
+  float normp = length(p - celestial.pos);
+  float c = (normp * normp) - (celestial.radius * celestial.radius);
   float discriminant = (prod * prod) - (4.0 * c);
   if (discriminant >= 0.0) {
     // determine intersection point
@@ -85,10 +127,9 @@ void drawSphere(vec3 centerPos, float radius, int texIndex) {
       }
 
       vec3 ipoint = (p + (t * dir));
-      vec3 normal = normalize(ipoint - centerPos);
+      vec3 normal = normalize(ipoint - celestial.pos);
 
       // determine texture coordinates in spherical coordinates
-
       // First rotate about x through 90 degrees so that y is up.
       normal.z = -normal.z;
       normal = normal.xzy;
@@ -101,43 +142,16 @@ void drawSphere(vec3 centerPos, float radius, int texIndex) {
         theta = atan(normal.y, normal.x);
       }
 
-      // Axial rotations
-      if (texIndex == TEXTURE_MOON || texIndex == TEXTURE_SUN) {
-        theta += pc.time / AXIAL_PERIOD_SUN_MOON;
-      } else if (texIndex == TEXTURE_EARTH) {
-        theta += pc.time / AXIAL_PERIOD_EARTH;
+      if (celestial.axialPeriod > 0.0) {
+        // Axial rotations
+        theta += pc.time / celestial.axialPeriod;
       }
 
       // normalize coordinates for texture sampling.
       // Top-left of texture is (0,0) in Vulkan, so we can stick to spherical
       // coordinates
-      color =
-          texture(textures[texIndex], vec2(1.0 + 0.5 * theta / PI, phi / PI));
+      color = texture(textures[celestial.texIndex],
+                      vec2(1.0 + 0.5 * theta / PI, phi / PI));
     }
   }
-}
-
-void main() {
-  color = vec4(bg_color, 1.0);
-
-  // distance from Earth -> Sun
-  float earthOrbitalRadius = 2.0;
-  // distance from Moon -> Earth
-  float moonOrbitalRadius = 0.5;
-
-  vec3 earthCenter =
-      earthOrbitalRadius * vec3(cos(pc.time / ORBITAL_PERIOD_EARTH), 0.0,
-                                sin(pc.time / ORBITAL_PERIOD_EARTH));
-  // Earth + moon orbit component
-  vec3 moonCenter =
-      earthCenter +
-      moonOrbitalRadius * vec3(cos(pc.time / ORBITAL_PERIOD_MOON), 0.0,
-                               sin(pc.time / ORBITAL_PERIOD_MOON));
-
-  // So that it does not get cut off
-  float skyBoxRadius = largestT * 0.99;
-  drawSphere(vec3(0), skyBoxRadius, TEXTURE_STARS);
-  drawSphere(vec3(0), 0.25, TEXTURE_SUN);
-  drawSphere(earthCenter, 0.2, TEXTURE_EARTH);
-  drawSphere(moonCenter, 0.08, TEXTURE_MOON);
 }
